040268ecd271d83cc68c747abc40e3286be979cc
Mounith M <125748918+mounithmouni@users.noreply.github.com>
2025-05-06 19:01:52 +0530

    Added .env

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
+let key = import.meta.env.VITE_API_KEY;
 const instance = axios.create({
-  baseURL:
-    "https://v6.exchangerate-api.com/v6/c6a1f078b28f498f7e7342c4/latest/",
+  baseURL: "https://v6.exchangerate-api.com/v6/" + key + "/latest/",
 });
 
 export default function getExchangeRate(value = "USD") {
